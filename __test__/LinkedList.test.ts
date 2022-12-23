9f48aa544e0876ddcf78b2a2397671b3dd011656
Asumie-code <darkeangel.1998.ia@gmail.com>
2022-12-23 18:57:19 +0100

    test: add insert, removetail, removehead tests

--- __test__/LinkedList.test.ts
+++ __test__/LinkedList.test.ts
@@ -40,5 +40,52 @@ test('prepend values to linkedList', () => {
     expect(list.head?.next).not.toBeNull()
     expect(list.length).toEqual(2)
     expect(list.head?.value).toEqual(3)
-} )
+})
+
+
+
+test('insert value to linkedList', () => {
+    let list: LinkedList<number> = new LinkedList(1 ,2 ,3)
+    let values = [1, 2, 4, 3]
+    list.insert(4, 2)
+    expect(list.head).not.toBeNull()
+    expect(list.tail).not.toBeNull()
+    expect(list.tail?.value).toEqual(3)
+
+    let index = 0
+    for(let value of list) {
+        expect(value).toEqual(values[index])
+        index++
+    }
+
+})
+
+
+
+test('remove head from linkedList', () => {
+    let list: LinkedList<number> = new LinkedList(1, 2, 3)
+
+    
+    list.removeHead()
+    expect(list.head).not.toBeNull()
+    expect(list.tail).not.toBeNull()
+    expect(list.head?.value).toEqual(2)
+    expect(list.tail?.value).toEqual(3)
+
+})
+
+
+
+
+test('remove tail from linkedList', () => {
+    let list: LinkedList<number> = new LinkedList(1, 2, 3)
+
+    
+    list.removeTail()
+    expect(list.head).not.toBeNull()
+    expect(list.tail).not.toBeNull()
+    expect(list.head?.value).toEqual(1)
+    expect(list.tail?.value).toEqual(2)
+
+})
 
